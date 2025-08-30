1832ad44ddda0d71cddbfdd58b58712bcb52e249
SUAD MALIK <suadmalik27@gmail.com>
2025-08-30 19:53:59 +0530

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# /backend/app/models/__init__.py (Updated)
-from .user import User
-from .medication import Medication
-from .appointment import Appointment
-from .contact import Contact
-from .tip import Tip
-from .medication_log import MedicationLog # <-- BAS YEH LINE ADD KARNI HAI
